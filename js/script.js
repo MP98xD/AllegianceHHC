71df27691f5324f9f0588aef9296f2d0d3cf1995
MosabJ <mosab.basil@gmail.com>
2018-07-23 09:18:51 -0600

    Added scroll animation and events handler for scroller controls;

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// JavaScript Document
+$(function (){
+	"use strict";
+	var scrolling = false;
+	$("#scrollUp").click(function () {
+		fscroll('up');
+		return false;
+	});
+	$("#scrollDown").click(function () {
+		fscroll('down');
+		return false;
+	});
+	$(document).bind('mousewheel DOMMouseScroll', function(e){
+        if (e.originalEvent.wheelDelta /120 > 0) {
+			fscroll('up');
+			return false;
+        } else {
+			fscroll('down');
+			return false;
+        }
+    });
+	
+	function fscroll(direction) {
+	if (!scrolling) {
+		scrolling = true;
+		$('html, body').animate({
+			'scrollTop': ((direction==='up')? '+=-' : '-=-')+ $(".page").height()+'px' 
+		}, 1500, "easeOutExpo", function () {scrolling = false;});
+	}
+}
+});
