8a41a4fce286adf09d2b4e98129e1bc36397b3fd
MosabJ <6750905+MP98xD@users.noreply.github.com>
2019-02-16 13:17:30 -0700

    changed map marker location and zoom level

--- js/script.js
+++ js/script.js
@@ -24,10 +24,10 @@ $(function (){
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat: 43.6138689, lng: -116.2430427};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 15, center: uluru});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
